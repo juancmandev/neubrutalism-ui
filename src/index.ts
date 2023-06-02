9dcd7318f433a0d13807803de2f886c9e3a59a57
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2023-06-02 17:27:14 -0600

    add export to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 export { NBButton } from './components/NBButton';
+export { NBBox } from './components/NBBox';
+export { NBDialog } from './components/NBDialog';
+export { NBDropdown } from './components/NBDropdown';
+export { NBMenuItem } from './components/NBMenuItem';
+
+export type { NBMenuItemProps } from './components/NBMenuItem';
+export type { DropdownProps } from './components/NBDropdown';
+export type { NBDialogProps } from './components/NBDialog';
+export type { NBBoxProps } from './components/NBBox';
 export type { NBButtonProps } from './components/NBButton';
