58c950dadac8e6b4537ffc4d0a097a2dd6539bc8
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2023-06-03 20:47:12 -0600

    add NB Input to index exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './components/NBBox';
 export * from './components/NBDialog';
 export * from './components/NBDropdown';
 export * from './components/NBDropdownActionable';
+export * from './components/NBInput';
